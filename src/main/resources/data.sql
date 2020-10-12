c94ef34bd9e8f8b709523629351ff3a12c231f77
joaobenites <joaofelipe22.jf@gmail.com>
2020-10-11 22:22:25 -0300

    Create data.sql

---
+++ src/main/resources/data.sql
@@ -0,0 +1,6 @@
+insert into user(email, is_enabled, name, password, phone,register_date, surname, username) Values
+("dev@teste.com",true,"teste","123","+5548988449955",now(),"Dev","teste_dev");
+insert into user(email, is_enabled, name, password, phone,register_date, surname, username) Values
+("joao@teste.com",true,"Joao","123123","+5551999532451",now(),"Felipe","joao_felipe");
+insert into user(email, is_enabled, name, password, phone,register_date, surname, username) Values
+("teste@teste.com",true,"example","1232","+5548988449955",now(),"test","example_test");
