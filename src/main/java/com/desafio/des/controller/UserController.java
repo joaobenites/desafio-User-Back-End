73242643302e1f174fe40e6f1a53aae80bb6d396
joaobenites <joaofelipe22.jf@gmail.com>
2020-10-11 22:22:33 -0300

    Update UserController.java

--- src/main/java/com/desafio/des/controller/UserController.java
+++ src/main/java/com/desafio/des/controller/UserController.java
@@ -3,6 +3,9 @@ package com.desafio.des.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.repository.query.Param;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -21,17 +24,47 @@ public class UserController {
    private UserService userService;
 
    @GetMapping
-   public List<User> findAll(){
-      return userService.getUsers();
+   public ResponseEntity<Object> findAll(){
+      try {
+		List<User> users = userService.getUsers();
+		return ResponseEntity.ok().body(users);
+	} catch (Exception e) {		
+		return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Erro ao buscar lista de usuário");
+	}
    }
    
    @PostMapping
-   public User createUser(@RequestBody User user) {
-	   user = this.userService.createUser(user);
-	   return user;
+   public ResponseEntity<Object> createUser(@RequestBody User user) {
+	   try {
+		user = this.userService.createUser(user);
+		return ResponseEntity.ok().body(user);
+	} catch (Exception e) {
+		// TODO Auto-generated catch block
+		return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Erro ao criar usuário");
+	}
    }
    
-   
+  @GetMapping("/delete")
+  public ResponseEntity<Void> deleteUser(@Param("id") Long id) {
+		try {
+			this.userService.deleteUser(id);
+			return ResponseEntity.ok().build();
+		} catch (Exception e) {
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+		}
+
+	}
+  
+  @PostMapping("/update")
+  public ResponseEntity<Void> updateUser(@RequestBody User user) {
+		try {
+			this.userService.updateUser(user);
+			return ResponseEntity.ok().build();
+		} catch (Exception e) {
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+		}
+
+	}
    
    
    
